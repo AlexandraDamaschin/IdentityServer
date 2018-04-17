f35df439a0d9e657f6daaeb346a79ad1cf71d262
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-17 20:37:02 +0100

    first commit

---
+++ Fiver.Security.AuthServer.Client/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Fiver.Security.AuthServer.Client.Models.Home;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Fiver.Security.AuthServer.Client.Controllers
+{
+    //controller to acces the api
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Movies()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            //calling the api
+            var content = await client.GetStringAsync("http://localhost:5001/movies/get");
+
+            var model = JsonConvert.DeserializeObject<List<MovieViewModel>>(content);
+            
+            return View(model);
+        }
+
+        public IActionResult Claims()
+        {
+            return View();
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
